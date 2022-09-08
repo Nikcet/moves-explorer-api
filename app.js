0ee56ac0130b46fca6e9bdf595a06e16a08b50b3
Nikcet <mstislav.kr@yandex.ru>
2022-09-08 17:55:02 +0300

    Added logs

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ const express = require('express');
 const { default: mongoose } = require('mongoose');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
 // const cors = require('cors');
+
 const {
   loginRouter,
   userRouter,
   moviesRouter,
 } = require('./routes/index');
-
 const { auth } = require('./middlewares/auth');
-// const bcrypt = require('bcryptjs');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-error');
 
 const { PORT = 3000 } = process.env;
@@ -30,13 +31,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(helmet());
 
+app.use(requestLogger);
+
 app.use('/', loginRouter);
 app.use('/', userRouter);
 app.use('/', moviesRouter);
 
+app.use(errorLogger);
+
 app.use(auth, (req, res, next) => {
   next(new NotFoundError('Not found url'));
 });
+
+app.use(errors());
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
