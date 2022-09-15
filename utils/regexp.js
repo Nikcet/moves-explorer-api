a4202e7ee1645062ad4ac8d41d53eaa28bc5f1f4
Nikcet <mstislav.kr@yandex.ru>
2022-09-16 01:23:33 +0300

    useless regexp, deleted

--- utils/regexp.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable no-useless-escape */
-/* eslint-disable prefer-regex-literals */
-const linkRegExp = new RegExp(/(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?$#?/);
-module.exports = linkRegExp;
