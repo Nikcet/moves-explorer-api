9cfad49130ecc8e13ed91a7fd3fda2a463b51b0c
Nikcet <mstislav.kr@yandex.ru>
2022-09-16 01:23:00 +0300

    validation fixes

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 const {
   getMovies,
   postMovie,
   deleteMovie,
 } = require('../controllers/movies');
-
-const linkRegExp = require('../utils/regexp');
 
 const { auth } = require('../middlewares/auth');
 
@@ -19,13 +18,27 @@ router.post('/movies', auth, celebrate({
     duration: Joi.number().integer().required(),
     year: Joi.string().required(),
     description: Joi.string().required(),
-    image: Joi.string().min(2).pattern(linkRegExp).required(),
-    trailerLink: Joi.string().min(2).pattern(linkRegExp).required(),
-    thumbnail: Joi.string().required(),
+    image: Joi.string().min(2).required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('Image field is not valid');
+    }),
+    trailerLink: Joi.string().min(2).required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('TrailerLink field is not valid');
+    }),
+    thumbnail: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) {
+        return value;
+      }
+      return helpers.message('TrailerLink field is not valid');
+    }),
     nameRU: Joi.string().required(),
     nameEN: Joi.string().required(),
-    movieId: Joi.string().length(36),
-    owner: Joi.string().length(24).hex(),
+    movieId: Joi.number().required(),
   }),
 }), postMovie);
 
